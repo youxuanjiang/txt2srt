2443a2fc96a19e71a0f1218006916b7a370920ad
江友軒 <jiangyouxuan@jiangyouxuandeMacBook-Pro.local>
2024-03-16 02:22:43 +0800

    1. 更改模式名稱，讓使用者更好理解 2. 在計時器模式中新增「時間歸零」 3. 新增Youtube模式

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const convertTimingButton = document.getElementById("convert-timing");
 const secondInput = document.getElementById("secondInput");
 const convertedPreview = document.getElementById("convertedPreview");
 const currentState = document.getElementById("current-state");
+const youtubeMode = document.getElementById("youtube_mode");
+const inputHint = document.getElementById("input_hint");
+const youtubePlayer = document.getElementById('youtubePlayer');
+const refershTimingButton = document.getElementById('refresh-timing');
 
 let fileContent = "";
 let lines;
@@ -19,6 +23,14 @@ let previousTime = 0;
 let isGeneratingSRT = false;
 let srtIndexForCustomize = 1;
 let contentIndex = 0;
+let player;
+let isVideoPlaying = false;
+
+// Youtube api
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 fileInput.addEventListener('change', function() {
     handleFileSelect();
@@ -26,28 +38,56 @@ fileInput.addEventListener('change', function() {
 
 fixInterval.addEventListener('change', function (){
     if(fixInterval.checked) {
+        refershTimingButton.style.display = 'none';
         cancelAnimationFrame(requestId);
         elapsedTime = 0;
         isTiming = false;
+        isGeneratingSRT = false;
+        isVideoPlaying = false;
         secondInput.disabled = false;
         secondInput.value = "";
         convertTimingButton.textContent = "轉換";
+        inputHint.textContent = "秒數：";
+        destroyPlayer();
     }
 });
 
 customizeInterval.addEventListener('change', function() {
     if(customizeInterval.checked) {
+        refershTimingButton.style.display = 'inline-block';
         secondInput.disabled = true;
+        isGeneratingSRT = false;
+        isVideoPlaying = false;
         secondInput.value = "00:00:00.000";
         convertTimingButton.textContent = "計時開始";
+        inputHint.textContent = "秒數：";
+        destroyPlayer();
     }
 })
+
+youtubeMode.addEventListener('change', function (){
+    if(youtubeMode.checked) {
+        refershTimingButton.style.display = 'none';
+        cancelAnimationFrame(requestId);
+        elapsedTime = 0;
+        isTiming = false;
+        isGeneratingSRT = false;
+        secondInput.disabled = false;
+        secondInput.value = "";
+        convertTimingButton.textContent = "嵌入影片";
+        inputHint.textContent = "YoutubeID："
+    }
+});
 
 document.addEventListener('keydown', function(event) {
     if (customizeInterval.checked) {
         if (event.key == 's') {
             timing();
-        } 
+        }
+
+        if (event.key == '0') {
+            refreshTiming();
+        }
         
         // 文件要先讀取進來才有辦法開始記錄SRT
         if (fileContent.length != 0) {
@@ -63,6 +103,22 @@ document.addEventListener('keydown', function(event) {
             }
         }
         
+    }
+
+    if (youtubeMode.checked) {
+        // 文件要先讀取進來才有辦法開始記錄SRT
+        if (fileContent.length != 0) {
+            if (event.key == 'a') { // start reading line
+                genSRTFromYoutube();
+                isGeneratingSRT = true;
+                currentState.textContent = "正在讀取：" + lines[contentIndex];
+
+            } else if (event.key == 'b') { // break from reading line
+                genSRTFromYoutube();
+                isGeneratingSRT = false;
+                currentState.textContent = "暫停讀取";
+            }
+        }
     }
     
     // Log the key code when a key is pressed
@@ -90,6 +146,8 @@ function handleCovertTimingBtn() {
         convert();
     } else if (customizeInterval.checked) {
         timing();
+    } else if (youtubeMode.checked) {
+        youtubeEmbed();
     }
 }
 
@@ -134,6 +192,13 @@ function formattedTimeForCustomize(time) {
     return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds)}`;
 }
 
+function formattedTimeForYoutube(time) {
+    const milliseconds = Math.floor((time % 1) * 1000);
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor((time / 60) % 60);
+    const hours = Math.floor((time / (60 * 60)) % 24);
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds)}`;
+}
 
 function timing() {
     isTiming = !isTiming;
@@ -146,11 +211,79 @@ function timing() {
     }
 }
 
+function refreshTiming() {
+    cancelAnimationFrame(requestId);
+    isTiming = true; // 為了方便做的調整，讓呼叫timing之後可以確定保持在「暫停」的狀態
+    timing();
+    elapsedTime = 0;
+    secondInput.value = "00:00:00.000";
+}
+
+function youtubeEmbed() {
+    destroyPlayer();
+    const youtube_id = secondInput.value;
+    if(youtube_id === '') {
+        alert("要輸入影片的ID唷！");
+        return;
+    }
+    player = new YT.Player('youtubePlayer', {
+        height: '390',
+        width: '640',
+        videoId: youtube_id,
+        playerVars: {
+          'playsinline': 1
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Player is ready, you can perform additional actions here if needed
+    setInterval(getCurrentTime, 1); // Update current time every millisecond
+}
+
+function destroyPlayer() {
+    if (player) {
+        player.destroy();
+        player = null; // Reset player variable
+        console.log('Player destroyed.');
+    } else {
+        console.log('Player not initialized.');
+    }
+}
+
+function onPlayerStateChange(event) {
+    isVideoPlaying = event.data == YT.PlayerState.PLAYING;
+}
+
+function getCurrentTime() {
+    if (player && isVideoPlaying) {
+        currentTime = player.getCurrentTime();
+    }
+}
+
 function genSRTFromTimer() {
     if(isGeneratingSRT && contentIndex<lines.length) {
         let srtContent = "";
         srtContent += srtIndexForCustomize + "\n";
         srtContent += formattedTimeForCustomize(previousTime) + " --> " + formattedTimeForCustomize(currentTime) + "\n";
+        srtContent += lines[contentIndex].trim() + "\n\n";
+        document.getElementById("convertedPreview").textContent += srtContent;
+        srtIndexForCustomize++;
+        contentIndex++;
+        convertedPreview.scrollTop = convertedPreview.scrollHeight;
+    }
+    previousTime = currentTime;
+}
+
+function genSRTFromYoutube() {
+    if(isGeneratingSRT && contentIndex<lines.length) {
+        let srtContent = "";
+        srtContent += srtIndexForCustomize + "\n";
+        srtContent += formattedTimeForYoutube(previousTime) + " --> " + formattedTimeForYoutube(currentTime) + "\n";
         srtContent += lines[contentIndex].trim() + "\n\n";
         document.getElementById("convertedPreview").textContent += srtContent;
         srtIndexForCustomize++;
